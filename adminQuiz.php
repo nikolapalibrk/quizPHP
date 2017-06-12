9bd8851d20216ab892149c14d5e3421af973cc2f
nikolapalibrk <palibrknikola@gmail.com>
2017-06-12 10:24:29 +0200

    Dodao coll efekat na linkove i promeio boju logo:hover

--- adminQuiz.php
+++ adminQuiz.php
@@ -16,11 +16,12 @@
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
      <link rel="stylesheet" href="css/main.min.css">
+<link href="https://fonts.googleapis.com/css?family=Bitter|Mogra" rel="stylesheet">
    </head>
 
    <body id="adminBody">
       <nav>
-        <a href="adminQuiz.php">Quiz App</a>
+        <a href="adminQuiz.php" id="logo">Quiz App</a>
         <ul>
           <li><a href="addQuestion.php">Add Questions</a></li>
           <li><a href="addAdmin.php">Add Admin</a></li>
--- index.php
+++ index.php
@@ -9,11 +9,12 @@
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
      <link rel="stylesheet" href="css/main.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Bitter|Mogra" rel="stylesheet">
    </head>
 
    <body id="indexBody">
       <nav>
-        <a href="index.php">Quiz App</a>
+        <a href="index.php" id="logo">Quiz App</a>
         <ul>
           <li><a href="login.php">Login</a></li>
         </ul>
